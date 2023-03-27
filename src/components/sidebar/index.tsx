13018c0e6bee05110eb5add2f61a41b6399f74d3
Michal Dyczkowski <themich4@gmail.com>
2023-03-27 18:15:44 +0200

    Sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,43 +1,19 @@
-import SidebarButton from "./sidebar-button";
+import { DarkThemeToggle, Sidebar as SB } from "flowbite-react";
 
 const Sidebar = () => {
   return (
-    <>
-      <button
-        data-drawer-target="default-sidebar"
-        data-drawer-toggle="default-sidebar"
-        aria-controls="default-sidebar"
-        type="button"
-        className="mt-2 ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-      >
-        <span className="sr-only">Open sidebar</span>
-        <svg
-          className="h-6 w-6"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-          />
-        </svg>
-      </button>
-
-      <aside
-        id="sidebar"
-        aria-label="Sidebar"
-        className="fixed top-0 left-0 z-40 h-screen w-64 -translate-x-full transition-transform sm:translate-x-0"
-      >
-        <div className="h-full overflow-y-auto bg-gray-50 px-3 py-4 dark:bg-gray-800">
-          <ul className="space-y-2 font-medium">
-            <SidebarButton />
-          </ul>
-        </div>
-      </aside>
-    </>
+    <aside className="fixed top-0 left-0 z-40 h-screen w-64 -translate-x-full  bg-red-200 transition-transform sm:translate-x-0">
+      <SB collapsed={false}>
+        <SB.Items>
+          <SB.ItemGroup>
+            <SB.Item>
+              <DarkThemeToggle />
+            </SB.Item>
+            <SB.Item>Log in</SB.Item>
+          </SB.ItemGroup>
+        </SB.Items>
+      </SB>
+    </aside>
   );
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
 
 module.exports = config;
