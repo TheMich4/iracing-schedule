4408ccca3a34eafa29538a1e87e6059d23f806de
Michal Dyczkowski <themich4@gmail.com>
2023-03-27 17:56:20 +0200

    Initial commit

---
+++ src/components/sidebar/sidebar-button.tsx
@@ -0,0 +1,24 @@
+const SidebarButton = () => {
+  return (
+    <li>
+      <a
+        href="#"
+        className="flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+      >
+        <svg
+          aria-hidden="true"
+          className="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
+          <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
+        </svg>
+        <span className="ml-3">Dashboard</span>
+      </a>
+    </li>
+  );
+};
+
+export default SidebarButton;
