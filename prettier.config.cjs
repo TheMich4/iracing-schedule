a9e2ffa4c161f2d76ff25f75435a826726433823
Michal Dyczkowski <themich4@gmail.com>
2023-03-27 18:39:19 +0200

    Added import order

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  importOrder: ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
 
 module.exports = config;
