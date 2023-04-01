c274dec0cf26db88876c0073837e4bb0b4ae7563
Michal Dyczkowski <themich4@gmail.com>
2023-04-01 14:36:37 +0200

    Table scroll wrapper

--- src/components/schedule-table/index.tsx
+++ src/components/schedule-table/index.tsx
@@ -74,49 +74,51 @@ const ScheduleTable = () => {
           setDate={setDate}
         />
       </div>
-      <table className="min-w-full divide-y divide-slate-700 rounded-sm">
-        <thead className="bg-slate-900/40">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th
-                  className="px-4 py-1 text-start text-xs font-medium uppercase tracking-wider text-gray-500"
-                  key={header.id}
-                >
-                  {header.isPlaceholder ? null : (
-                    <div
-                      className={`flex flex-row items-center gap-1 ${
-                        header.column.getCanSort() ? "cursor-pointer" : ""
-                      }`}
-                      onClick={header.column.getToggleSortingHandler()}
-                    >
-                      {flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                      <SortIcon isSorted={header.column.getIsSorted()} />
-                    </div>
-                  )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody className="divide-y divide-slate-800">
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td
-                  className="whitespace-nowrap px-4 py-1 text-sm text-slate-200"
-                  key={cell.id}
-                >
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="w-full overflow-x-auto">
+        <table className="min-w-full divide-y divide-slate-700 rounded-sm">
+          <thead className="bg-slate-900/40">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    className="px-4 py-1 text-start text-xs font-medium uppercase tracking-wider text-gray-500"
+                    key={header.id}
+                  >
+                    {header.isPlaceholder ? null : (
+                      <div
+                        className={`flex flex-row items-center gap-1 ${
+                          header.column.getCanSort() ? "cursor-pointer" : ""
+                        }`}
+                        onClick={header.column.getToggleSortingHandler()}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        <SortIcon isSorted={header.column.getIsSorted()} />
+                      </div>
+                    )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody className="divide-y divide-slate-800">
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    className="whitespace-nowrap px-4 py-1 text-sm text-slate-200"
+                    key={cell.id}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
