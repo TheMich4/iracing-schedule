d42c36cafab8ac7219e83f6eb04ca43dbb42c066
Michal Dyczkowski <themich4@gmail.com>
2023-04-03 20:02:46 +0200

    Default license group filters

--- src/hooks/use-filter/index.ts
+++ src/hooks/use-filter/index.ts
@@ -1,12 +1,13 @@
 import type { Dispatch, SetStateAction } from "react";
 
 import type { Filter } from "./types";
+import { licenseGroups } from "~/utils/license";
 import { trackTypes } from "~/utils/track-type";
 import { useState } from "react";
 
 const useFilter = (): [Filter, Dispatch<SetStateAction<Filter>>] => {
   const [filter, setFilter] = useState<Filter>({
-    licenseGroup: [],
+    licenseGroup: Object.keys(licenseGroups),
     official: ["Official", "Unofficial"],
     setup: ["Fixed", "Open"],
     trackType: Object.keys(trackTypes),
