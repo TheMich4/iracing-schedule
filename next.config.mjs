55ab6c439a6b213d9fdc91c571a0e4f16c6d28a6
Michal Dyczkowski <themich4@gmail.com>
2023-04-11 20:46:42 +0200

    Added turbopack and temporary disabled production errors

--- next.config.mjs
+++ next.config.mjs
@@ -14,9 +14,23 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
+
+  // TODO: Disable those in production
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 export default config;
