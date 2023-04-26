9d64a8acb8d432ceeb67f42fa9ff8810a62e383c
Michał Dyczkowski <themich4@gmail.com>
2023-04-26 19:00:07 +0200

    Unify icons to lucide library (#4)
    * Removed leftover code
    
    * Unified icon library to lucide

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,9 +1,9 @@
-import { Bars3Icon } from "@heroicons/react/24/outline";
+import { MenuIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center gap-2 rounded-b-md bg-slate-900 p-2 lg:hidden">
-      <Bars3Icon className="h-6 w-6 cursor-pointer rounded-sm  hover:bg-slate-800" />
+      <MenuIcon className="h-6 w-6 cursor-pointer rounded-sm  hover:bg-slate-800" />
       <div className="">iRacing Schedule</div>
     </nav>
   );
