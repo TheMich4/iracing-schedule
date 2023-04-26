61dd2d56cba3a89aca75e3f82487b3518a9a9fc5
Michał Dyczkowski <themich4@gmail.com>
2023-04-26 17:34:46 +0200

    Add shadcn styles (#2)
    * Added shadcn/ui styles
    
    * Added theme toggle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,27 +2,30 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import Sidebar from "~/components/sidebar";
-
+import { ThemeProvider } from "next-themes";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import Navbar from "~/components/navbar";
 
+// TODO: Remove defaultTheme when styling is done
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <div>
-        <div className="flex w-screen flex-row justify-between">
-          <Sidebar />
-          <div className="flex h-screen w-full flex-col overflow-auto">
-            <Navbar />
-            <Component {...pageProps} />
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <div>
+          <div className="flex w-screen flex-row justify-between">
+            <Sidebar />
+            <div className="flex h-screen w-full flex-col overflow-auto">
+              <Navbar />
+              <Component {...pageProps} />
+            </div>
           </div>
         </div>
-      </div>
+      </ThemeProvider>
     </SessionProvider>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,8 @@ const config = {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
-  plugins: [require("tailwind-scrollbar")],
+  darkMode: ["class"],
+  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar")],
   safelist: [
     // For class backgrounds
     "bg-red-500",
@@ -14,9 +15,70 @@ const config = {
     "bg-blue-500",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
+      },
       fontFamily: {
         kanit: ["Kanit"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
