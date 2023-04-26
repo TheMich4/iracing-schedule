1d308b06e0bcedc11407f6ad7c3d2e04688c91ed
Michal Dyczkowski <themich4@gmail.com>
2023-04-26 19:57:08 +0200

    Fixed hydration issue

--- src/components/sidebar/sidebar-button.tsx
+++ src/components/sidebar/sidebar-button.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import type { SidebarButtonProps } from "./types";
 
 const SidebarButton = ({
@@ -6,12 +9,19 @@ const SidebarButton = ({
   Icon,
   onClick,
 }: SidebarButtonProps) => {
+  // TODO: Find better solution for hydration issue
+  const [showIcon, setShowIcon] = useState(false);
+
+  useEffect(() => {
+    setShowIcon(true);
+  }, []);
+
   return (
     <button
       className="flex h-10 w-full cursor-pointer flex-row items-center gap-2 rounded-md p-2 text-start font-medium hover:bg-slate-300/40 dark:hover:bg-slate-800/40"
       onClick={onClick}
     >
-      <Icon className="w-5-200 h-5 dark:text-slate-100" />
+      {showIcon && <Icon className="w-5-200 h-5 dark:text-slate-100" />}
       {expanded && children}
     </button>
   );
