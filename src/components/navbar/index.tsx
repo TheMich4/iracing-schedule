e781f11f135ab40407d790b108d0a15837a7550d
Michal Dyczkowski <themich4@gmail.com>
2023-04-26 22:06:02 +0200

    Navbar styling

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,13 @@
 import { MenuIcon } from "lucide-react";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
-    <nav className="flex items-center gap-2 rounded-b-md bg-slate-900 p-2 lg:hidden">
-      <MenuIcon className="h-6 w-6 cursor-pointer rounded-sm  hover:bg-slate-800" />
-      <div className="">iRacing Schedule</div>
+    <nav className="flex items-center gap-2 rounded-b-md bg-slate-200  p-2 dark:bg-slate-900 lg:hidden">
+      <MenuIcon className="h-6 w-6 cursor-pointer rounded-sm hover:bg-slate-300/40 dark:hover:bg-slate-800/40" />
+      <div onClick={() => void router.push("/")}>iRacing Schedule</div>
     </nav>
   );
 };
