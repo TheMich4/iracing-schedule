0fb5339232a020cde0a1e1f2a4d4e14dbfd7dcf2
Michal Dyczkowski <themich4@gmail.com>
2023-05-09 20:05:04 +0200

    Fixed isAdmin non defined

--- src/components/layout/navbar/navbar-menu.tsx
+++ src/components/layout/navbar/navbar-menu.tsx
@@ -22,7 +22,7 @@ const NavbarMenu = ({ user }: { user: User }) => {
           <NavbarButton label="Schedule" pathname="/" />
           {user && <NavbarButton label="Profile" pathname="/profile" />}
 
-          {user.isAdmin && <NavbarButton label="Admin" pathname="/admin" />}
+          {user?.isAdmin && <NavbarButton label="Admin" pathname="/admin" />}
 
           <LoginButton user={user} />
         </div>
