7f7921150e33bd24bb872e9f6c6cd93f9efdfa20
Michal Dyczkowski <themich4@gmail.com>
2023-05-15 18:38:57 +0200

    Added favorite track context menu

--- src/hooks/use-columns/cells/track-cell.tsx
+++ src/hooks/use-columns/cells/track-cell.tsx
@@ -1,3 +1,11 @@
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "~/components/ui/context-menu";
+
+import { Star } from "lucide-react";
 import cn from "~/utils/cn";
 import { useMemo } from "react";
 
@@ -13,11 +21,29 @@ const TrackCell = ({ row, content }) => {
     return content?.tracks?.[trackId]?.owned ?? false;
   }, [trackId, content]);
 
+  const isFavorite = useMemo(() => {
+    return content?.tracks?.[trackId]?.favorite ?? false;
+  }, [trackId, content]);
+
+  // TODO: Fix full width
   return (
     <div
-      className={cn(isOwned && "-m-2 bg-green-100/40 p-2 dark:bg-green-900/40")}
+      className={cn(
+        isOwned && "-m-2 w-full bg-green-100/40 p-2 dark:bg-green-900/40"
+      )}
     >
-      {trackName ?? "Unknown"}
+      <ContextMenu>
+        <ContextMenuTrigger className=" cursor-pointer">
+          {trackName ?? "Unknown"}
+        </ContextMenuTrigger>
+
+        <ContextMenuContent>
+          <ContextMenuItem>
+            <Star className="mr-2 h-4 w-4" />
+            {isFavorite ? "Remove favorite track" : "Add favorite track"}
+          </ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
     </div>
   );
 };
