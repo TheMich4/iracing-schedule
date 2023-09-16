c7eb818c581f4a46f977b0777765b8e73a7579e4
Michal Dyczkowski <themich4@gmail.com>
2023-09-16 14:53:42 +0200

    Theme toggle

---
+++ src/components/layout/sidebar.tsx
@@ -0,0 +1,73 @@
+import {
+  CalendarIcon,
+  DotsHorizontalIcon,
+  GearIcon,
+  HomeIcon,
+} from "@radix-ui/react-icons";
+
+import { Button } from "../ui/button";
+import { IconProps } from "@radix-ui/react-icons/dist/types";
+import { ThemeSwitch } from "../theme-switch";
+
+const SidebarButton = ({
+  children,
+  IconComponent,
+}: {
+  children: React.ReactNode;
+  IconComponent: React.ForwardRefExoticComponent<
+    IconProps & React.RefAttributes<SVGSVGElement>
+  >;
+}) => {
+  return (
+    <li>
+      <Button asChild className="w-full" variant="ghost">
+        <a
+          href="#"
+          className="flex items-center rounded-md px-3 py-2 text-foreground hover:bg-accent"
+        >
+          <IconComponent className="h-[1.2rem] w-[1.2rem]" />
+          <span className="ml-3 flex-1 whitespace-nowrap ">{children}</span>
+        </a>
+      </Button>
+    </li>
+  );
+};
+
+export const Sidebar = () => {
+  return (
+    <aside
+      id="sidebar"
+      className="fixed left-0 top-0 z-40 h-screen w-64 transition-transform"
+      aria-label="Sidebar"
+    >
+      <div className="flex h-full flex-col overflow-y-auto border-r  bg-background px-3 py-4">
+        <a
+          href="#"
+          className="mb-10 flex items-center rounded-lg px-3 py-2 text-foreground"
+        >
+          <CalendarIcon className="h-5 w-5" />
+          <span className="ml-3 text-base font-semibold">iRacing Schedule</span>
+        </a>
+        <ul className="space-y-2 text-sm font-medium">
+          <SidebarButton IconComponent={HomeIcon}>Home</SidebarButton>
+          <SidebarButton IconComponent={GearIcon}>Settings</SidebarButton>
+        </ul>
+
+        <div className="mt-auto flex flex-col gap-2">
+          <div>
+            <ThemeSwitch />
+          </div>
+
+          <div className="flex flex-row w-full">
+            <div className="flex w-full justify-between">
+              <span className="text-sm font-medium text-foreground">
+                email@example.com
+              </span>
+              <DotsHorizontalIcon className="h-5 w-5" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
+};
---
+++ src/components/theme-switch.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import * as React from "react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+
+export function ThemeSwitch() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="w-full flex flex-row items-start">
+          <div className="flex">
+            <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          </div>
+          <p className="ml-3 whitespace-nowrap flex-1 text-start">Theme</p>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center" side="top">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
