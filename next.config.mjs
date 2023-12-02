4385cd84dd7cd0b8cdf75613d237e4c7a99d20a3
Michal Dyczkowski <themich4@gmail.com>
2023-12-02 14:05:59 +0100

    Add TypeScript ignoreBuildErrors option to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@
 await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
 
 export default config;
