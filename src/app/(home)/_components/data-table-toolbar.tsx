5c325b0fcd8ec4415cdd044996596de50dd647f8
Michal Dyczkowski <themich4@gmail.com>
2023-12-24 13:12:51 +0100

    Refactor DataTableToolbar component

--- src/app/(home)/_components/data-table-toolbar.tsx
+++ src/app/(home)/_components/data-table-toolbar.tsx
@@ -21,71 +21,75 @@ export const DataTableToolbar = ({ table }: DataTableToolbarProps) => {
 
   return (
     <div className="flex items-center justify-between">
-      <div className="flex flex-1 items-center space-x-2">
-        <Input
-          placeholder="Filter series..."
-          value={
-            (table.getColumn("seriesName")?.getFilterValue() as string) ?? ""
-          }
-          onChange={(event) =>
-            table.getColumn("seriesName")?.setFilterValue(event.target.value)
-          }
-          className="h-8 w-[150px] lg:w-[250px]"
-        />
-        <Input
-          placeholder="Filter tracks..."
-          value={(table.getColumn("track")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("track")?.setFilterValue(event.target.value)
-          }
-          className="h-8 w-[150px] lg:w-[250px]"
-        />
+      <div className="flex flex-1 flex-col justify-center gap-1">
+        <div className="flex flex-row gap-2">
+          <Input
+            placeholder="Filter series..."
+            value={
+              (table.getColumn("seriesName")?.getFilterValue() as string) ?? ""
+            }
+            onChange={(event) =>
+              table.getColumn("seriesName")?.setFilterValue(event.target.value)
+            }
+            className="h-8 w-[150px] lg:w-[250px]"
+          />
+          <Input
+            placeholder="Filter tracks..."
+            value={(table.getColumn("track")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              table.getColumn("track")?.setFilterValue(event.target.value)
+            }
+            className="h-8 w-[150px] lg:w-[250px]"
+          />
+        </div>
 
-        {table.getColumn("class") && (
-          <DataTableFacetedFilter
-            column={table.getColumn("class")}
-            title="Class"
-            options={classOptions}
-          />
-        )}
-        {table.getColumn("category") && (
-          <DataTableFacetedFilter
-            column={table.getColumn("category")}
-            title="Category"
-            options={categoryOptions}
-          />
-        )}
-        {table.getColumn("multiClass") && (
-          <DataTableFacetedFilter
-            column={table.getColumn("multiClass")}
-            title="Multi-Class"
-            options={multiClassOptions}
-          />
-        )}
-        {table.getColumn("official") && (
-          <DataTableFacetedFilter
-            column={table.getColumn("official")}
-            title="Official"
-            options={officialOptions}
-          />
-        )}
-        {table.getColumn("fixed") && (
-          <DataTableFacetedFilter
-            column={table.getColumn("fixed")}
-            title="Setup"
-            options={setupOptions}
-          />
-        )}
-        {isFiltered && (
-          <Button
-            variant="ghost"
-            onClick={() => table.resetColumnFilters()}
-            className="h-8 px-2 lg:px-3"
-          >
-            Reset
-            <Cross2Icon className="ml-2 h-4 w-4" />
-          </Button>
-        )}
+        <div className="inline-block space-x-2 space-y-1">
+          {table.getColumn("class") && (
+            <DataTableFacetedFilter
+              column={table.getColumn("class")}
+              title="Class"
+              options={classOptions}
+            />
+          )}
+          {table.getColumn("category") && (
+            <DataTableFacetedFilter
+              column={table.getColumn("category")}
+              title="Category"
+              options={categoryOptions}
+            />
+          )}
+          {table.getColumn("multiClass") && (
+            <DataTableFacetedFilter
+              column={table.getColumn("multiClass")}
+              title="Multi-Class"
+              options={multiClassOptions}
+            />
+          )}
+          {table.getColumn("official") && (
+            <DataTableFacetedFilter
+              column={table.getColumn("official")}
+              title="Official"
+              options={officialOptions}
+            />
+          )}
+          {table.getColumn("fixed") && (
+            <DataTableFacetedFilter
+              column={table.getColumn("fixed")}
+              title="Setup"
+              options={setupOptions}
+            />
+          )}
+          {isFiltered && (
+            <Button
+              variant="ghost"
+              onClick={() => table.resetColumnFilters()}
+              className="h-8  px-2 lg:px-3"
+            >
+              Reset
+              <Cross2Icon className="ml-2 h-4 w-4" />
+            </Button>
+          )}
+        </div>
       </div>
       {/* <DataTableViewOptions table={table} /> */}
     </div>
