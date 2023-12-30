dddd193edf5d89629fcae396ac166ff91dc48894
Michal Dyczkowski <themich4@gmail.com>
2023-12-30 08:53:22 +0100

    Remove console.log statements

--- src/app/track/[trackId]/page.tsx
+++ src/app/track/[trackId]/page.tsx
@@ -11,7 +11,7 @@ export default async function TrackPage({
   params: { trackId },
 }: TrackPageProps) {
   const track = await api.track.get.query(+trackId);
-  console.log({ trackId, track });
+
   return (
     <main className="container flex flex-col gap-2 bg-background py-4">
       <div className="text-2xl font-bold">{`${track.trackName} - ${track.configName}`}</div>
--- src/server/api/routers/track.ts
+++ src/server/api/routers/track.ts
@@ -6,7 +6,6 @@ import { z } from "zod";
 
 export const trackRouter = createTRPCRouter({
   get: publicProcedure.input(z.number()).query(({ input }) => {
-    console.log({ input });
     return tracks.find((track) => track.trackId === input) as Track;
   }),
 });
