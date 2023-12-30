1812a89141b16e247e24fbc490fd946b49bba53d
Michal Dyczkowski <themich4@gmail.com>
2023-12-30 08:08:20 +0100

    Update iracing-api

--- src/server/api/routers/track.ts
+++ src/server/api/routers/track.ts
@@ -1,11 +1,12 @@
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 import tracks from "@/data/tracks.json";
+import { type Track } from "iracing-api";
 import { z } from "zod";
 
 export const trackRouter = createTRPCRouter({
   get: publicProcedure.input(z.number()).query(({ input }) => {
     console.log({ input });
-    return tracks.find((track) => track.trackId === input);
+    return tracks.find((track) => track.trackId === input) as Track;
   }),
 });
