92784942b613e339176e764e5c11874d7f32e42e
Michal Dyczkowski <themich4@gmail.com>
2024-03-12 16:00:27 +0100

    Add eslint ignoreDuringBuilds option and fix searchParams null check

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
