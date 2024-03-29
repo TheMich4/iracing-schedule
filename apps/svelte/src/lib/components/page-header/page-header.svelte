5d4336e4c637ee845f7464901a9374509cfd3b20
Michal Dyczkowski <themich4@gmail.com>
2024-03-29 10:04:34 +0100

    Add new icons and update page header component

--- apps/svelte/src/lib/components/page-header/page-header.svelte
+++ apps/svelte/src/lib/components/page-header/page-header.svelte
@@ -1,11 +1,47 @@
 <script>
-	import { IconDotsVertical } from '@tabler/icons-svelte';
+	import {
+		IconDotsVertical,
+		IconCalendarEvent,
+		IconLayoutSidebarLeftExpand,
+		IconMinusVertical
+	} from '@tabler/icons-svelte';
+
+	import sidebar from '$lib/store/sidebar.svelte';
 </script>
 
 <div class="border-b-[1px] border-stone-200 p-2 px-4">
 	<div class="flex items-center justify-between">
 		<div class="flex items-center gap-2">
-			<slot />
+			<div class="flex items-center gap-2">
+				{#if !sidebar.isOpen}
+					<div class="flex items-center gap-2 md:hidden">
+						<button
+							class="flex items-center opacity-60 transition hover:opacity-100 md:hidden"
+							on:click={sidebar.toggle}
+						>
+							<IconLayoutSidebarLeftExpand class="size-4" />
+						</button>
+						<IconMinusVertical class="opacity-30" />
+					</div>
+				{/if}
+
+				{#if sidebar.isCollapsed}
+					<div class="hidden items-center gap-2 md:flex">
+						<button
+							class="flex items-center opacity-60 transition hover:opacity-100"
+							on:click={sidebar.toggleCollapse}
+						>
+							<IconLayoutSidebarLeftExpand class="size-4" />
+						</button>
+						<IconMinusVertical class="size-4 opacity-30" />
+					</div>
+				{/if}
+
+				<div class="flex items-center gap-1">
+					<IconCalendarEvent class="size-4" />
+					<span class="font-semibold">Schedule</span>
+				</div>
+			</div>
 		</div>
 
 		<div class="flex items-center gap-2">
--- apps/svelte/src/routes/+page.svelte
+++ apps/svelte/src/routes/+page.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
 	import PageHeader from '@/components/page-header';
-	import {
-		IconCalendarEvent,
-		IconLayoutSidebarLeftExpand,
-		IconMinusVertical
-	} from '@tabler/icons-svelte';
-
-	import sidebar from '$lib/store/sidebar.svelte.js';
 	import Table from '$lib/components/table/table.svelte';
 	import { columns } from './columns';
 	import TableFilters from './table-filters.svelte';
@@ -15,38 +8,7 @@
 </script>
 
 <div class="h-screen">
-	<PageHeader>
-		<div class="flex items-center gap-2">
-			{#if !sidebar.isOpen}
-				<div class="flex items-center gap-2 md:hidden">
-					<button
-						class="flex items-center opacity-60 transition hover:opacity-100 md:hidden"
-						on:click={sidebar.toggle}
-					>
-						<IconLayoutSidebarLeftExpand class="size-4" />
-					</button>
-					<IconMinusVertical class="opacity-30" />
-				</div>
-			{/if}
-
-			{#if sidebar.isCollapsed}
-				<div class="hidden items-center gap-2 md:flex">
-					<button
-						class="flex items-center opacity-60 transition hover:opacity-100"
-						on:click={sidebar.toggleCollapse}
-					>
-						<IconLayoutSidebarLeftExpand class="size-4" />
-					</button>
-					<IconMinusVertical class="size-4 opacity-30" />
-				</div>
-			{/if}
-
-			<div class="flex items-center gap-1">
-				<IconCalendarEvent class="size-4" />
-				<span class="font-semibold">Schedule</span>
-			</div>
-		</div>
-	</PageHeader>
+	<PageHeader />
 
 	<div class="h-[calc(100%-42px)] flex-1 overflow-hidden">
 		<div class="flex h-full flex-col">
