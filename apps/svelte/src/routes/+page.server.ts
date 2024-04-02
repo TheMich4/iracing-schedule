08d52899f66b4514bc5636b53c3808ad5d97cfe7
Michal Dyczkowski <themich4@gmail.com>
2024-04-02 11:59:03 +0200

    Added utils package

--- apps/svelte/src/routes/+page.server.ts
+++ apps/svelte/src/routes/+page.server.ts
@@ -1,8 +1,12 @@
+import { getPreviousTuesdayString } from '@iracing-schedule/utils';
 import seasonsData from '$lib/seasons-data.json';
 
 export function load() {
+	const date = new Date();
+	const tuesday = getPreviousTuesdayString(date);
+	//
 	// @ts-expect-error Add type
-	const weekData = seasonsData['2024-04-02'];
+	const weekData = seasonsData[tuesday] ?? [];
 
 	return {
 		weekData
