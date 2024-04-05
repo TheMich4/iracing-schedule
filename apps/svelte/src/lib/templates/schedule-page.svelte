85b69e893ad743fb20018b41dd3498f3156a9bf5
Michal Dyczkowski <themich4@gmail.com>
2024-04-05 16:55:43 +0200

    Created SchedulePage template

---
+++ apps/svelte/src/lib/templates/schedule-page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import PageHeader from '@/components/page-header/page-header.svelte';
+	import TableFilters from '@/components/table/table-filters.svelte';
+	import Table from '@/components/table/table.svelte';
+
+	export let columns: Array<Record<string, any>> = [];
+	export let data: Array<Record<string, any>> = [];
+	export let title: string;
+</script>
+
+<div class="h-screen">
+	<PageHeader {title} />
+
+	<div class="h-[calc(100%-42px)] flex-1 overflow-hidden">
+		<div class="flex h-full flex-col">
+			<TableFilters />
+
+			<div class="relative h-full w-full flex-1 overflow-auto px-4">
+				<Table rows={data} {columns} />
+			</div>
+		</div>
+	</div>
+</div>
--- apps/svelte/src/routes/(home)/+page.svelte
+++ apps/svelte/src/routes/(home)/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import PageHeader from '@/components/page-header';
-	import Table from '$lib/components/table/table.svelte';
 	import { columns } from './columns';
-	import TableFilters from '$lib/components/table/table-filters.svelte';
 	import { getScheduleState } from '$lib/store/schedule.svelte';
 	import { getPreviousTuesdayString } from '@iracing-schedule/utils';
+	import SchedulePage from '$lib/templates/schedule-page.svelte';
 
 	let schedule = getScheduleState();
 
@@ -13,16 +11,4 @@
 	let weekData = $derived(data.schedule[getPreviousTuesdayString(schedule.date)]);
 </script>
 
-<div class="h-screen">
-	<PageHeader title="Schedule" />
-
-	<div class="h-[calc(100%-42px)] flex-1 overflow-hidden">
-		<div class="flex h-full flex-col">
-			<TableFilters />
-
-			<div class="relative h-full w-full flex-1 overflow-auto px-4">
-				<Table rows={weekData} {columns} />
-			</div>
-		</div>
-	</div>
-</div>
+<SchedulePage data={weekData} {columns} title="Schedule" />
