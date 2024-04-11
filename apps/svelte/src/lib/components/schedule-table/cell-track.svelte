16c5ba2a9c2c5eeadd8aa7581d3defeb751b2b95
Michal Dyczkowski <themich4@gmail.com>
2024-04-11 18:15:49 +0200

    Refactor track column in cell-track.svelte

--- apps/svelte/src/lib/components/schedule-table/cell-track.svelte
+++ apps/svelte/src/lib/components/schedule-table/cell-track.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import FreeIcon from '../free-icon.svelte';
 	import type { WeekEntry } from '@iracing-schedule/data';
-	import type { Column } from '@/templates/column';
 
-	export let column: Column;
 	export let row: WeekEntry;
 
-	$: track = column.getValue(row);
+	$: track = row.track;
 </script>
 
 <div class="group flex flex-row flex-wrap items-baseline gap-1 text-wrap">
