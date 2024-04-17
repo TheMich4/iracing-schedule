4aec3080f8c38ca2c152ef6b031c67475fffcf29
Michal Dyczkowski <themich4@gmail.com>
2024-04-17 18:25:32 +0200

    Refactor sidebar quick actions component in Svelte app

---
+++ apps/svelte/src/lib/components/sidebar/sidebar-button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import { page } from '$app/stores';
+
+	export let href: string;
+	export let Icon: any | null = null;
+
+	$: currentRoute = $page.url.pathname;
+</script>
+
+<a
+	class={cn(
+		'flex items-center gap-2 rounded-md px-2 py-1 text-sm font-medium',
+		currentRoute === href && 'bg-muted-foreground/10'
+	)}
+	{href}
+>
+	{#if Icon}
+		<Icon class="size-4 opacity-70" />
+	{/if}
+	<span>Schedule</span>
+</a>
--- apps/svelte/src/lib/components/sidebar/sidebar-quick-actions.svelte
+++ apps/svelte/src/lib/components/sidebar/sidebar-quick-actions.svelte
@@ -1,36 +1,21 @@
 <script>
 	import { IconCalendarEvent, IconStar } from '@tabler/icons-svelte';
 	import { page } from '$app/stores';
-	import { cn } from '../../utils';
+	import SidebarButton from './sidebar-button.svelte';
 
 	$: currentRoute = $page.url.pathname;
 </script>
 
 <div class="flex flex-col gap-2">
-	<a
-		class={cn(
-			'flex items-center gap-2 rounded-md px-2 py-1 text-sm font-medium',
-			currentRoute === '/' && 'active'
-		)}
-		href="/"
-	>
-		<IconCalendarEvent class="size-4 opacity-70" />
-		<span>Schedule</span>
-	</a>
-<!--	<a-->
-<!--		class={cn(-->
-<!--			'flex items-center gap-2 rounded-md px-2 py-1 text-sm font-medium',-->
-<!--			currentRoute === '/favorite' && 'active'-->
-<!--		)}-->
-<!--		href="/favorite"-->
-<!--	>-->
-<!--		<IconStar class="size-4 opacity-70" />-->
-<!--		<span>Favorite</span>-->
-<!--	</a>-->
+	<SidebarButton Icon={IconCalendarEvent} href="/" />
+	<!--	<a-->
+	<!--		class={cn(-->
+	<!--			'flex items-center gap-2 rounded-md px-2 py-1 text-sm font-medium',-->
+	<!--			currentRoute === '/favorite' && 'active'-->
+	<!--		)}-->
+	<!--		href="/favorite"-->
+	<!--	>-->
+	<!--		<IconStar class="size-4 opacity-70" />-->
+	<!--		<span>Favorite</span>-->
+	<!--	</a>-->
 </div>
-
-<style lang="pcss">
-	.active {
-		@apply bg-stone-900/5;
-	}
-</style>
