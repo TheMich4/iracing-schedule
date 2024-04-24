bb9d86fe3d800c5b847758dafc0bebed0d807684
Michal Dyczkowski <themich4@gmail.com>
2024-04-24 17:00:51 +0200

    Theme button styling

--- apps/svelte/src/lib/components/theme-switch.svelte
+++ apps/svelte/src/lib/components/theme-switch.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	import Sun from 'svelte-radix/Sun.svelte';
 	import Moon from 'svelte-radix/Moon.svelte';
-	import { toggleMode } from 'mode-watcher';
-	import { Button } from '$lib/components/ui/button/index.js';
+	import { toggleMode, mode } from 'mode-watcher';
 </script>
 
-<Button
+<button
 	on:click={toggleMode}
-	variant="ghost"
-	size="icon"
-	class="flex w-full flex-row items-center justify-items-start gap-2 px-2 py-1"
+	class="flex w-full flex-row items-center justify-items-start gap-2 rounded-md px-2 py-1 text-sm font-medium transition-all hover:bg-muted-foreground/10"
 >
-	<Sun class="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-	<Moon class="rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+	{#if $mode === 'dark'}
+		<Moon class="rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+	{:else}
+		<Sun class="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+	{/if}
 	<span>Toggle theme</span>
-</Button>
+</button>
