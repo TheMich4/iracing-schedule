0ba4961b11ddb5a5daf953ffd9ce831c91a26d8e
Michal Dyczkowski <themich4@gmail.com>
2024-04-27 09:18:44 +0200

    Favorite context menu

--- apps/svelte/src/lib/components/schedule-table/cell-favorite.svelte
+++ apps/svelte/src/lib/components/schedule-table/cell-favorite.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import * as ContextMenu from '$lib/components/ui/context-menu';
 	import FavoriteIcon from '../favorite-icon.svelte';
 	import type { FavoriteState } from '@/config/favorite';
 
@@ -22,9 +23,23 @@
 	};
 </script>
 
-<a class="group inline-flex cursor-pointer flex-row items-baseline gap-1" on:click={handleClick}>
-	{#if isFavorite}
-		<FavoriteIcon />
-	{/if}
-	<slot />
-</a>
+<ContextMenu.Root>
+	<ContextMenu.Trigger>
+		<div class="group inline-flex cursor-pointer flex-row items-baseline gap-1">
+			{#if isFavorite}
+				<FavoriteIcon />
+			{/if}
+			<slot />
+		</div>
+	</ContextMenu.Trigger>
+
+	<ContextMenu.Content>
+		<ContextMenu.Item on:click={handleClick}>
+			{#if isFavorite}
+				Unfavorite
+			{:else}
+				Favorite
+			{/if}
+		</ContextMenu.Item>
+	</ContextMenu.Content>
+</ContextMenu.Root>
